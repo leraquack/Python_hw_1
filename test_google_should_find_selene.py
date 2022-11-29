9e21497d9242670d5f73140a55b39b04a3c3f063
leraquack <leralera009@gmail.com>
2022-11-29 13:09:02 +0100

    fix errors

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -20,9 +20,7 @@ def test_google_result(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene git').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
 
-search_query_negative = 'Jhbhfgvkdjfvjdlfvkndf'
-search_result_negative = 'По запросу - {search_query_negative} - ничего не найдено.'
 
 def test_negative_google_result(open_browser):
-    browser.element('[name="q"]').should(be.blank).type('Jhbhfgvkdjfvjdlfvkndf').press_enter()
+    browser.element('[name="q"]').should(be.blank).type("Jhbhfgvkdjfvjdlfvkndf").press_enter()
     browser.element('[id="rcnt"]').should(have.text('По запросу Jhbhfgvkdjfvjdlfvkndf ничего не найдено.'))
